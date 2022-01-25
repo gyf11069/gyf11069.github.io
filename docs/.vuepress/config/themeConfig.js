88108fb32c1a9f6d1a651d8ce5dfb7a5ef9f8f42
gyf11069 <1224254097@qq.com>
2022-01-25 13:13:15 +0800

    更新博客内容 + GitHub Actions 自动部署

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -81,7 +81,7 @@ module.exports = {
     // 页脚信息
     createYear: 2021, // 博客创建年份
     copyrightInfo:
-      '莫哈德 | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>' + '<br> <a href="http://beian.miit.gov.cn/" target="_blank">桂ICP备2021009994号</a>', // 博客版权信息，支持a标签
+      '莫哈德 | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>' + '<br> <a href="http://beian.miit.gov.cn/" target="_blank">赣ICP备2022000686号</a>', // 博客版权信息，支持a标签
   },
   // htmlModules // 插入hmtl(广告)模块
 }
