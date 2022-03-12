ff4db2d90ada2c999d2f2cd41db000d3c0e6cfde
gyf11069 <1224254097@qq.com>
2022-03-13 00:34:01 +0800

    更新博客内容 + GitHub Actions 自动部署

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -55,7 +55,7 @@ module.exports = [
     items: [
       {text: 'Java',link: '/pages/6c2c13/'},
       {text: 'JavaWeb',link: '/pages/6b9079/'},
-      {text: 'Mysql',link: '/pages/b0b425/'},
+      {text: 'MySQL',link: '/pages/b0b425/'},
       {text: 'Spring',link: '/pages/538344/'},
     ],
   },
@@ -67,7 +67,7 @@ module.exports = [
       { text:'Git命令',link: '/pages/548b44/'},
       { text: '面试', link: '/pages/aea6571b7a8bae86/' },
       { text:'博客搭建', link:'/pages/85c53a/'},
-      { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
+      { text: '解忧杂货店', link: '/pages/2d615df9a36a98ed/' },
       { text: '实用技巧', link: '/pages/baaa02/' },
       { text: '友情链接', link: '/friends/' },
     ],
@@ -86,11 +86,6 @@ module.exports = [
   {
     text: '收藏',
     link: '/pages/beb6c0bd8a66cea6/',
-    // items: [
-    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-    // ],
   },
 
   { text: '关于', link: '/about/' },
